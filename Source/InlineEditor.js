81011a11c7a5ec5219ed404104c2997f35c89770
Nathan Reed <reednj@gmail.com>
2010-09-03 09:54:22 +0800

    updated javascript headers, included util functions in main js file

--- Source/InlineEditor.js
+++ Source/InlineEditor.js
@@ -10,7 +10,9 @@ authors:
 requires:
 - core/1.2.4: '*'
 
-provides: InlineEditor
+provides: 
+- InlineEditor
+- $e
 
 ...
 */
@@ -169,3 +171,75 @@ var InlineEditor = new Class({
 	}
 });
 
+//
+// $e(): Use the mootools new element function to chain up element creation in a nice way
+//
+// eg. $e('b', 'bold text'); -> <b>bold text</b>
+//     $e('a', {'href': 'http://www.google.com', 'text': 'google'}); -> <a href='http://www.google.com'>google</a>
+//
+// A more complex example using children:
+//
+//    $e('a', {
+//       'href': './home',
+//       'children': [
+//          $e('img', {'src': './logo.png', 'title': 'popacular'}),
+//          $e('span', 'popacular.com/home')
+//       ]
+//    });
+//
+// gives:
+// <a href='./home'>
+//    <img src='./logo.png' title='popacular' />
+//    <span>popacular.com/home<span>
+//  </a>
+//
+// Created:  2010-05-21
+// License: MIT-Style License
+// Nathan Reed (c) 2010
+//
+function $e(tag, props) {
+   tag = tag || 'div';
+
+   if(!$defined(props)) {
+      return new Element(tag);
+   }
+
+   // normalize the properties element for the
+   // mootools element constructor
+   if($type(props) == 'string') {
+      props = {'text': props};
+   } else if($type(props) == 'element') {
+      props = {'children': props};
+   }
+
+   // remove the children property from the array, we don't want it in there.
+   // because when we pass these properties to the mootools element function it
+   // might get confused.
+   var children = props.children;
+   props.children = null;
+
+   var new_element = new Element(tag, props);
+
+   if($defined(children)) {
+
+      if($type(children) == 'element') {
+         // if they have just passed through one child, then
+         // normalize it by turning it into an array with one element.
+         children = [children];
+      }
+
+      // add the children to the new element one by one
+      children.each(function(item) {
+         new_element.grab(item);
+      });
+
+   }
+
+   return new_element
+}
+
+// this is now implemented in mootool.more (finally!)
+Element.implement({
+   show: function() {this.setStyle('display','');},
+   hide: function() {this.setStyle('display','none');}
+});
