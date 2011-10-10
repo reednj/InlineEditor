675dec04f07bf6418d562abd8da7289a7d8e5a32
Pete Allison <pete@ecrayon.net>
2011-10-10 11:43:18 +0100

    Fixed a bug where fixed decimal places were not maintained if the decimal placing ended with a 0 (zero).

--- Source/InlineEditor.js
+++ Source/InlineEditor.js
@@ -327,7 +327,7 @@ Number.implement({ format: function(decimals, dec_point, thousands_sep) {
 		var matches = /(-)?(\d+)(\.\d+)?/.exec((isNaN(this) ? 0 : this) + ''); // returns matches[1] as sign, matches[2] as numbers and matches[3] as decimals
 		var remainder = matches[2].length > 3 ? matches[2].length % 3 : 0;
 		return (matches[1] ? matches[1] : '') + (remainder ? matches[2].substr(0, remainder) + thousands_sep : '') + matches[2].substr(remainder).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep) +
-				(decimals && matches[3]? dec_point + (+ matches[3] || 0).round(decimals).toString().substr(2) : '');
+				(decimals ? dec_point + (+ matches[3] || 0).toFixed(decimals).toString().substr(2) : '');
 
 }});
 
